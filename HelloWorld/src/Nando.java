17bdd0dc86f86ccd9e4b5447a566858ab26ed7c7
Fernando Silva <fernando111922@hotmail.com>
2024-03-23 16:30:56 -0300

    Gallons conversion Liters.

--- HelloWorld/src/Nando.java
+++ HelloWorld/src/Nando.java
@@ -3,8 +3,11 @@ public class Nando {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("blz");
-
+		System.out.println("Hello World");
+				
+	}
+	public static void galToLit() {
+		
 	}
 
 }
