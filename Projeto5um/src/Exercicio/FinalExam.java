90ea119e97ed8f86d8d0a0d4d8dcc630a7200c37
fernando-dev97 <fernando111922@hotmail.com>
2024-04-06 15:30:48 -0300

    Exercicio 5.2

--- Projeto5um/src/Exercicio/FinalExam.java
+++ Projeto5um/src/Exercicio/FinalExam.java
@@ -3,21 +3,17 @@ package Exercicio;
 import java.util.Scanner;
 
 public class FinalExam {
+	
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 	
-	int x = 0, y = 7, z = 7;
-	int total =x+y;
-	System.out.print(x == y);
+	int x = 7, y = 0, z = 7;
 	
-	{
-	if (x<=7);
-	System.out.println("Verdadeiro");
-	}
+	String resposta = x <=7 ? "Verdadeiro" : "Falso";
+	System.out.println(resposta);
 	
-	{
-	else (y>=7);
-			
-	}
+    
+	
 	
 	
 	}
