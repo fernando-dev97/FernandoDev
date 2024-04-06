858b90baf62ce6238cd10a55cd5e4693b16e0ea8
fernando-dev97 <fernando111922@hotmail.com>
2024-04-06 15:32:16 -0300

    Exercicio 5.2

--- Projeto5um/src/Exercicio/FinalExam.java
+++ Projeto5um/src/Exercicio/FinalExam.java
@@ -12,7 +12,14 @@ public class FinalExam {
 	String resposta = x <=7 ? "Verdadeiro" : "Falso";
 	System.out.println(resposta);
 	
-    
+    int resp = x <=7 ? 1 : 0;
+	
+	if(resp == 1) {
+		System.out.println("Verdadeiro");		
+	}
+	else if(resp ==0){
+		System.out.println("Falso");
+	}
 	
 	
 	
