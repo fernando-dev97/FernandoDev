64e1088bf939f3f52b91a7946e0779a0b13126b2
fernando-dev97 <fernando111922@hotmail.com>
2024-04-13 14:59:44 -0300

    Aula 13/04/2024

--- HelloWorld/src/Nando.java
+++ HelloWorld/src/Nando.java
@@ -2,12 +2,13 @@
 public class Nando {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Fernando");	
-		int x = 22, y = 10;
-		double p = Math.sqrt( ( x + y ) /2);
-		System.out.println(p);
-		
+		// TODO Auto-generated method stub	
+		int x = 0;
+		if ((5.1 > 4.3 && 6.2 < 8.4) && (7.2 < 3.5 || 1.2 == || != 2.25))
+			x=3;
+			else
+				x=4;
+		system.out.print(x);
 
 		
 						
